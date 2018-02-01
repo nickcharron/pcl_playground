23451fa083996a8c0fb621cc877af49c21b7e6ca
nickcharron <nicholas.c.charron@gmail.com>
2018-01-31 23:32:27 -0500

    Initial commit - adding icp and incremental icp

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # pcl_playground
 This repo is for learning and testing work with with PCL.
+
+Here are some of the things included:
+ - iterative_closest_point.cpp :
+      identical code that is in PCL tutorials, this is just for playing around
+      the code and figuring out how everything works
+
+ - incremental_iterative_closest_point.cpp:
+      This is a custom version of the incremental ICP code. I wrote it from
+      scratch to suit my needs and do everything the way I want.
+      It reads PCD point cloud files based on the naming, and plots the clouds
+      at each step including one viewport with each new scan compared to old
+      scan and one viewport showing the combined scan. You can keep visualizing
+      it step by step as it incrementally adds your clouds.
---
+++ src/incremental_iterative_closest_point.cpp
@@ -0,0 +1,100 @@
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/point_representation.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/registration/icp.h>
+#include <pcl/registration/icp_nl.h>
+#include <pcl/registration/transforms.h>
+#include <pcl/visualization/pcl_visualizer.h>
+
+//using pcl::visualization::PointCloudColorHandlerGenericField;
+typedef pcl::PointXYZ PointT;
+typedef pcl::PointCloud<PointT> PointCloud;
+
+// Global point cloud containers:
+  PointCloud::Ptr  cloud_out (new PointCloud);
+  PointCloud::Ptr  cloud_prev (new PointCloud);
+  PointCloud::Ptr  cloud_new (new PointCloud); // Cannot define these in for loop
+  PointCloud::Ptr  cloud_new_transformed (new PointCloud);
+  PointCloud::Ptr  cloud_result (new PointCloud);
+
+// Global matrix containers
+  Eigen::Matrix4f globalTransform = Eigen::Matrix4f::Identity ();
+
+  int main (int argc, char** argv)
+  {
+      // Initialize first cloud
+      pcl::PCLPointCloud2 cloud_intermediate;
+      pcl::io::loadPCDFile ("../pcds/bag3/pcd1.pcd", cloud_intermediate);
+      pcl::fromPCLPointCloud2 (cloud_intermediate, *cloud_out);
+      *cloud_new = *cloud_out;
+
+      Eigen::Matrix4f newToPrev, prevToNew;
+      Eigen::Matrix4f globalTransform = Eigen::Matrix4f::Identity ();
+
+
+      // Initialize visualizer
+      pcl::visualization::PCLVisualizer *p;
+      int vp1, vp2;
+      p = new pcl::visualization::PCLVisualizer (argc, argv, "Pairwise Incremental Registration Testing");
+      p->createViewPort (0.0, 0, 0.5, 1.0, vp1);
+      p->createViewPort (0.5, 0, 1.0, 1.0, vp2);
+      p->addCoordinateSystem (1.0);
+                                                                                //R,  G,  B
+      pcl::visualization::PointCloudColorHandlerCustom<PointT> prev_h(cloud_prev, 255, 0, 0);
+      pcl::visualization::PointCloudColorHandlerCustom<PointT> new_h (cloud_new, 0, 255, 0);
+      pcl::visualization::PointCloudColorHandlerCustom<PointT> new_transformed_h (cloud_new_transformed, 0, 0, 255);
+      pcl::visualization::PointCloudColorHandlerCustom<PointT> out_h (cloud_out, 255, 255, 255);
+
+      // Iterate over all point clouds - hard code
+      for (int i=2; i < 37; i++)
+      {
+          // Set prev cloud to new cloud before assigning another new cloud
+          *cloud_prev = *cloud_new;
+
+          // read and convert point cloud from pcd file
+          pcl::PCLPointCloud2 cloud_intermediate;
+          char pcdFileName[100];
+          sprintf(pcdFileName, "../pcds/bag3/pcd%d.pcd", i);
+          PCL_INFO ("Read PCD File: %s\n", pcdFileName);
+          pcl::io::loadPCDFile (pcdFileName, cloud_intermediate);
+          pcl::fromPCLPointCloud2 (cloud_intermediate, *cloud_new); //* convert from pcl/PCLPointCloud2 to pcl::PointCloud<T>
+
+          // perform icp
+          pcl::IterativeClosestPoint<PointT, PointT> icp;
+          icp.setInputSource(cloud_new);
+          icp.setInputTarget(cloud_prev);
+          icp.align(*cloud_result);
+
+          // Get transforms
+          newToPrev = icp.getFinalTransformation ();
+          //prevToNew = newToPrev.inverse();
+
+          // Calculate transform between new point cloud and original
+          globalTransform = globalTransform * newToPrev;
+          //globalTransform = globalTransform * newToPrev.inverse();
+
+          // Transform new cloud back in first cloud frame (original cloud)
+          pcl::transformPointCloud (*cloud_new, *cloud_new_transformed, globalTransform);
+
+          // Now that the new scan is in the frame of the first scan, add to map
+          *cloud_out += *cloud_new_transformed;
+
+          // visualize
+          p->removePointCloud ("vp_new");
+          p->removePointCloud ("vp_prev");
+          p->removePointCloud ("vp_out");
+          p->removePointCloud ("vp_new_transformed");
+
+          p->addPointCloud (cloud_new, new_h , "vp_new", vp1);
+          p->addPointCloud (cloud_prev, prev_h, "vp_prev", vp1);
+          p->addPointCloud (cloud_new_transformed, new_transformed_h, "vp_new_transformed", vp1);
+
+          p->addPointCloud (cloud_out, out_h, "vp_out", vp2);
+
+          PCL_INFO ("Press q to continue the registration.\n");
+          p-> spin();
+
+      }
+
+  }
